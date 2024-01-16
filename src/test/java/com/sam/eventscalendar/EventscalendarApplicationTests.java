cf551eb1d7778fb74236c1fb5e299ee4a36548bb
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2024-01-16 13:59:26 +1100

    first commit

---
+++ src/test/java/com/sam/eventscalendar/EventscalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.eventscalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventscalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
