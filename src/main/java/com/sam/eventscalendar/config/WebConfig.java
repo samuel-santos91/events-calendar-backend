53aec23069dd50f1fbd466a6d89f36f2ba7d5400
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2024-02-08 19:18:52 +1100

    adds deployed site domain and dockerfile

--- src/main/java/com/sam/eventscalendar/config/WebConfig.java
+++ src/main/java/com/sam/eventscalendar/config/WebConfig.java
@@ -9,6 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
   public void addCorsMappings(CorsRegistry registry) {
     String[] allowedOrigins = {
+      "https://events-calendar-sam.netlify.app/",
       "http://localhost:5173/",
       "http://127.0.0.1:5173/",
     };
