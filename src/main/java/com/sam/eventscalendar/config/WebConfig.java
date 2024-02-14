af7c9b7f61e4fe0603c69bfb8a58ec79ba1674c1
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2024-02-14 18:34:55 +1100

    removes localhost url

--- src/main/java/com/sam/eventscalendar/config/WebConfig.java
+++ src/main/java/com/sam/eventscalendar/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     String[] allowedOrigins = {
       "https://events-calendar-sam.netlify.app",
-      "http://localhost:5173",
-      "http://127.0.0.1:5173",
+      // "http://localhost:5173",
+      // "http://127.0.0.1:5173",
     };
 
     registry
